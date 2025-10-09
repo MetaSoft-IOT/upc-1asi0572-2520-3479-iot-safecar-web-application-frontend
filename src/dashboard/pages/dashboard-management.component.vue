97a7c82eb5f55fe65d230206b39fdbf38fe289fd
JanoverSaldana <u20201b510@upc.edu.pe>
2025-10-08 19:56:27 -0500

    feat(dashboard): add dashboard management component with module under development

---
+++ src/dashboard/pages/dashboard-management.component.vue
@@ -0,0 +1,38 @@
+<script>
+
+import ModuleUnderDevelopment from "@/public/components/module-under-development.component.vue";
+
+export default {
+  name: 'dashboard-management',
+  components: {ModuleUnderDevelopment},
+  data() {
+    return {
+      // Datos específicos del dashboard
+    };
+  },
+  methods: {
+    // Métodos específicos del dashboard
+  }
+};
+
+
+</script>
+
+<template>
+
+  <div class="dashboard-management">
+
+    <module-under-development
+        :module-name="$t('dashboard.module_name')"
+        :estimated-date="'26/10/2025'"
+        :show-progress="true"
+        :progress-percentage="15"
+    />
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
