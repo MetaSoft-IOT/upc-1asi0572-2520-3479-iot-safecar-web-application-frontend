f9ada6da0d1ddeea6deadf61d5d3f752dbd23b0f
JanoverSaldana <u20201b510@upc.edu.pe>
2025-10-08 21:28:58 -0500

    feat(vehicle-detail-management): create initial vehicle detail management component structure

---
+++ src/vehicle-management/pages/vehicle-detail-management.component.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: 'vehicle-detail-management',
+
+
+  data() {
+
+  },
+
+  methods: {},
+
+  created() {
+  },
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
