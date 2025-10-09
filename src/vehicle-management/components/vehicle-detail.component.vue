6d51fb818efbc9f1e3142e46da2f298c784f730c
JanoverSaldana <u20201b510@upc.edu.pe>
2025-10-08 21:28:48 -0500

    feat(vehicle-management): add vehicle detail and telemetry components with initial structure

---
+++ src/vehicle-management/components/vehicle-detail.component.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  name: 'vehicle-detail',
+
+  data() {
+    return {
+      vehicle: null,
+      loading: true,
+      error: null
+    };
+  },
+
+  methods: {},
+
+  created() {
+
+
+  },
+
+};
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
